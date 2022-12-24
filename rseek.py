b65e24b4772070baa54ca6f14e98274420e42861
3Xploitd <40121116+3Xploitd@users.noreply.github.com>
2022-12-24 02:20:55 -0600

    Update rseek.py
    Added regex file to pull common hard-coded secrets to search for and the `-s/--secrets` flag to search for those secrets. Added the `-a/--addregex` flag to add additional regexes to search for when running `-s/--secrets`

--- rseek.py
+++ rseek.py
@@ -1,33 +1,60 @@
 #!/usr/bin/python3
 
-import re,sys,argparse
+import re,sys,argparse,json
+from time import time
 
 from pathlib import Path
 
-def rseek(directory,filetype,text):
+def rseek(directory,filetype,regex):
     
     if filetype.lower() == 'all':
         filetype = '*'
+    if args.regex or args.text != None:
+        pattern = re.compile(regex)
+    start = time()
+    outfile = {}
+    
     for path in Path(directory).rglob('*.' + str(filetype)):
-        if args.regex != None:
-            search_string = r"{}".format(text)
-            
-        else:
-            search_string = r"{}".format(re.escape(text)) + r"{1,20}"
+        
         encoding_list = 'latin-1'
+        
         try:
+            
             f = open(path,'r',encoding=encoding_list).read()
-            regex = re.findall(search_string,f, re.IGNORECASE)
-            if regex != []:
-                print(set(regex),path)
-
+            if args.text or args.regex != None:
+                pattern = re.compile(regex)
+                result = pattern.findall(f, re.IGNORECASE)
+                if result != []:
+                    print("Found {} in {}".format(set(result),path))
+                    
+                        
+            else:# args.secrets == True:
+                get_secrets(regex,path,f)
+                        
         except KeyboardInterrupt:
-            print('KeyboardInterrupt Detected, exiting now....')
-            sys.exit(1)
+            print("Keyboard Interrupt Detected, exiting now...")
+            sys.exit(0)
+
         except Exception as e:
             continue
-            
-        
+    end = time()
+    totaltime = (end-start)/60
+    print("\nCompleted in {} minutes\n".format(str(totaltime)))
+def load_secrets():
+    f = open("regexes.json",'r').read()
+    regex = json.loads(f)
+    
+    return regex
+    
+
+def get_secrets(regex,path,f):
+
+    for search_pattern in regex:
+        pattern = re.compile(regex[search_pattern])
+        result = pattern.findall(f)
+        if result != []:
+            print("{}:\033[1;31;40m{}\033[0;0m Found in {}".format(search_pattern,list(set(result))[0],path))
+           
         
 if __name__ == '__main__':
     
@@ -36,16 +63,25 @@ if __name__ == '__main__':
     group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument('-t','--text',help='text or to search for',action='store')
     group.add_argument('-r','--regex',help='regular expression to match on',action='store')
+    group.add_argument('-s','--secrets',help='Secrets to search for',action='store_true')
     parser.add_argument('-d','--directory',help='The directory to search in',required=True,action='store')
-    parser.add_argument('-f','--filetype',help='The file extention to search for')
+    parser.add_argument('-f','--filetype',help='The file extention to search for',required=True,action='store')
+    parser.add_argument('-a','--addregex',help='Adds a regex expression to the secrets file',required=False,action='store')
+    
     args = parser.parse_args()
 
     directory = args.directory
     filetype = args.filetype
     if args.text != None:
-        text = args.text
+        regex = r'{}'.format(re.escape(args.text))
+
+    elif args.regex != None:
+        regex = r'{}'.format(args.regex)
+
     else:
-        text = args.regex
-        
-    rseek(directory,filetype,text)
-    
+        regex = load_secrets()
+        if args.addregex:
+            add_regex = args.addregex
+            regex[add_regex[:(add_regex.find(':'))]] = add_regex[(add_regex.find(':')+1):]
+       
+    rseek(directory,filetype,regex)
